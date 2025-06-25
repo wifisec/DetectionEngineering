8e7c67c7ddfe6fe90d219269ea70ce387e875e2d
Adair John Collins <adair.collins@gmail.com>
2025-06-25 14:41:55 -0400

    Update hw.ps1

--- hw.ps1
+++ hw.ps1
@@ -1,7 +1,1 @@
-Write-Output "Hello, world!"
-
-# Get the directory the script is running from
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-# Output the path to flag.txt in the same directory
-"$scriptDir" | Out-File -FilePath (Join-Path $scriptDir "flag.txt") -Encoding UTF8
+"Hello World" | Out-File -FilePath "flag.txt"
