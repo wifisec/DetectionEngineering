74fef3b5c745577ab010bf938f967aa82236d2c0
Adair John Collins <adair.collins@gmail.com>
2025-06-25 15:17:08 -0400

    Update hw.ps1

--- hw.ps1
+++ hw.ps1
@@ -1,1 +1,8 @@
-"Hello World" > "c:\windows\temp\flag.txt"'
+# Define the message and target path
+$message = "Hello World"
+$tempFolder = $env:WINDIR + "\Temp"
+$fileName = "flag.txt"
+$filePath = Join-Path -Path $tempFolder -ChildPath $fileName
+
+# Output the message to the console and the file
+$message | Tee-Object -FilePath $filePath
