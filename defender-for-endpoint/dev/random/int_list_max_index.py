966e44af39fb52f95a6bdf8db45f029a79c37da9
Adair John Collins <adair.collins@gmail.com>
2025-11-22 19:49:37 -0500

    Add script to find max value and index in list

---
+++ defender-for-endpoint/dev/random/int_list_max_index.py
@@ -0,0 +1,7 @@
+sample_int_array = [1, 2, 3, 4, 5]
+find_max_with_index = lambda num: (max(num), num.index(max(num)))
+largest, largest_index = find_max_with_index(sample_int_array)
+print(f"The largest value is {largest} and the index is {largest_index}")
+largest = max(sample_int_array)
+largest_index = sample_int_array.index(largest)
+print(f"The largest value is {largest} and the index is {largest_index}")
